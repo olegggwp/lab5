462d7df7b7775262637a5de89daffe30064037d4
olegggwp <boolvargosha@yandex.ru>
2024-01-06 05:15:53 +0300

    watch output last time when dead

--- watch1.sh
+++ watch1.sh
@@ -10,17 +10,21 @@ while true
 do
   top_table=$(top -bc -w 200 -n 1)
   info=$(grep "mem.sh" <<< $top_table)
-  if [[ -z $info ]]; 
-  then break; 
-  fi
   date +%H:%M:%S
   # выводит строчки про память и своп
   echo "$top_table" | head -n 5 | tail -n 2
-  echo $info
   # 5 строк процессов
   echo "$top_table" | head -n 12 | tail -n 5
+
+  if [[ -z $info ]]; 
+  then break; 
+  fi
+
+  echo $info
   echo "-----------------------------------"
   sleep 1
 done >> $log
 
 sudo dmesg | grep "Killed"
+
+sudo dmesg | grep "Killed" >> $log
